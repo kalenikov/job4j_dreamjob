ae06e0598bce2d4d79da580f47e5ad77fadfcde1
serj <sergeykalenikov@gmail.com>
2021-07-22 14:07:00 +0300

    init

---
+++ src/test/java/ru/job4j/AppTest.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void whenTest() {
+        assertTrue(true);
+    }
+}
