ffc269402e31e409d4af01d208fee9fc8738d203
serj <sergeykalenikov@gmail.com>
2021-07-23 10:30:57 +0300

    4. candidates.jsp - список кандидатов. [#276522]

--- src/main/java/ru/job4j/store/Store.java
+++ src/main/java/ru/job4j/store/Store.java
@@ -1,5 +1,6 @@
 package ru.job4j.store;
 
+import ru.job4j.model.Candidate;
 import ru.job4j.model.Post;
 
 import java.sql.Timestamp;
@@ -11,11 +12,15 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Store {
     private static final Store INST = new Store();
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
-        posts.put(1, new Post(1, "name1","desc1", Timestamp.valueOf(LocalDateTime.now())));
-        posts.put(2, new Post(2, "name2","desc2", Timestamp.valueOf(LocalDateTime.now())));
-        posts.put(3, new Post(3, "name3","desc3", Timestamp.valueOf(LocalDateTime.now())));
+        posts.put(1, new Post(1, "name1", "desc1", Timestamp.valueOf(LocalDateTime.now())));
+        posts.put(2, new Post(2, "name2", "desc2", Timestamp.valueOf(LocalDateTime.now())));
+        posts.put(3, new Post(3, "name3", "desc3", Timestamp.valueOf(LocalDateTime.now())));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
@@ -24,5 +29,9 @@ public class Store {
 
     public Collection<Post> findAll() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
