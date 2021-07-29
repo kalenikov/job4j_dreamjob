0f462179d7bfb46c93b10e439ab84c09b860e948
serj <sergeykalenikov@gmail.com>
2021-07-29 10:27:17 +0300

    fix

--- src/main/java/ru/job4j/servlet/IndexServlet.java
+++ src/main/java/ru/job4j/servlet/IndexServlet.java
@@ -1,7 +1,5 @@
 package ru.job4j.servlet;
 
-import ru.job4j.PropertySource;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
