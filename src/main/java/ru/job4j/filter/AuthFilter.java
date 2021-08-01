272473f2c81e6ca46eb56e665d1c4bdddcc9ea3a
serj <sergeykalenikov@gmail.com>
2021-08-01 19:54:21 +0300

    2. Filter [#2517]

---
+++ src/main/java/ru/job4j/filter/AuthFilter.java
@@ -0,0 +1,24 @@
+package ru.job4j.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String uri = req.getRequestURI();
+        if (uri.endsWith("auth.do")) {
+            chain.doFilter(request, response);
+            return;
+        }
+        if (req.getSession().getAttribute("user") == null) {
+            req.getRequestDispatcher("WEB-INF/jsp/login.jsp").forward(req, resp);
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+}
